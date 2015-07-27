ee0d51db24abe8a7b2313a1014ed4974300fc5eb
Cameron Yick <cameron.yick@yale.edu>
2015-07-27 04:44:46 -0400

    add fire search

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask import Flask, render_template, request
 # request used to detect request types and change request type
 
 import requests #pip make requests
-
+#from urllib import quote_plus # for encoding doesn't help
+#from requests.utils import quote
+#
 # Create an Flask app object.  We'll use this to create the routes.
 app = Flask(__name__)
 
@@ -28,6 +30,20 @@ def website():
 def search():
     if request.method == "POST":
         url = "https://api.github.com/search/repositories?q=" + request.form["user_search"]
+        response_dict = requests.get(url).json()
+        return render_template("results.html", api_data=response_dict)
+    else: # REQUEST METHOD == "GET"
+        return render_template("search.html")
+
+@app.route("/hartford", methods=["GET", "POST"])
+# @app.route("/hartford/", methods=["GET", "POST"])
+@app.route("/hartford/<dataId>", methods=["GET", "POST"]) # account for optional parameter
+def opendata(dataId = "824e-9vse"):# default to fire dataset
+    if request.method == "POST":
+        if request.form["user_search"] != "":
+            url = "https://data.hartford.gov/resource/"+ dataId +".json?" + request.form["user_search"]
+        else:
+            url = "https://data.hartford.gov/resource/"+ dataId +".json?alm_date=2015-07-11T00:00:00.000" #default
         response_dict = requests.get(url).json()
         return render_template("results.html", api_data=response_dict)
     else: # REQUEST METHOD == "GET"
---
+++ structure.md
@@ -0,0 +1,6 @@
+├── app.py          # Application logic
+├── static/         # Static content
+│   ├── css/        # CSS stylesheets
+│   ├── img/        # Images
+│   └── js/         # JavaScript files      
+└── templates/      # HTML pages
